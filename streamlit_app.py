d40a0ad1b5fcc7bebf3acbcaec81fbfdb664d84d
Emmad <62903078+Emmad-xD@users.noreply.github.com>
2023-05-02 03:16:15 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,104 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
+!pip install -Uqq fastai 
+from fastai.vision.all import *
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+"""At first we are downloading the Food Dataset, containing about 100k images of various kind of foods.
+Documentation for all Datasets: https://docs.fast.ai/data.external
 """
 
+foodPath = untar_data(URLs.FOOD)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+#How many images are we dealing with
+len(get_image_files(foodPath))
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+#Use Pandas to parse the JSON as a human readable table
+pd.read_json('/root/.fastai/data/food-101/test.json')
 
-    points_per_turn = total_points / num_turns
+#Deciding which two foods we want to classify
+labelA = 'samosa'
+labelB = 'hot_and_sour_soup'
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+"""**Some Data Preperation**
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+Next we're gonna do some preperation to make the data handling easier. It's not important that you understand the following code snippet completly.
+
+We're just doing two things:
+
+
+1.   Removing all images that don't have labelA or labelB
+2.   Renaming images with labelA or labelB to have that label in their filename
+
+
+"""
+
+#Loop through all Images downloaded
+for img in get_image_files(foodPath):    
+  #Rename Images so that the Label (Samosa or Churros) is in the file name
+  if labelA in str(img):
+    img.rename(f"{img.parent}/{labelA}-{img.name}")
+  elif labelB in str(img):
+    img.rename(f"{img.parent}/{labelB}-{img.name}")
+  else: os.remove(img) #If the Images are not part of labelA or labelB
+
+len(get_image_files(foodPath))
+
+"""## Train Model"""
+
+def GetLabel(fileName):
+  return fileName.split('-')[0]
+
+GetLabel("churros-734186.jpg") #Testing
+
+dls = ImageDataLoaders.from_name_func(
+    foodPath, get_image_files(foodPath), valid_pct=0.2, seed=420,
+    label_func=GetLabel, item_tfms=Resize(32))
+
+dls.train.show_batch()
+
+learn = cnn_learner(dls, resnet34, metrics=error_rate, pretrained=True)
+learn.fine_tune(epochs=1)
+
+"""Check out this while training to have look inside a Convolutional Neural Network.
+
+https://distill.pub/2017/feature-visualization/
+
+## Verify Model
+
+### Upload your own Images
+"""
+
+from google.colab import files
+uploader = files.upload()
+
+for img in uploader.items():
+  uploadedImg = img[0]
+
+img = PILImage.create(uploadedImg)
+img.show()
+
+label,_,probs = learn.predict(img)
+
+print(f"This is a {label}.")
+print(f"{labelA} {probs[1].item():.6f}")
+print(f"{labelB} {probs[0].item():.6f}")
+
+"""### Test using images in dataset"""
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+for i in range(0,10):
+  #Load random image
+  randomIndex = random.randint(0, len(get_image_files(foodPath))-1)
+  img = mpimg.imread(get_image_files(foodPath)[randomIndex])
+  #Put into Model
+  label,_,probs = learn.predict(img)
+
+  #Create Figure using Matplotlib
+  fig = plt.figure()
+  ax = fig.add_subplot() #Add Subplot (For multiple images)
+  imgplot = plt.imshow(img) #Add Image into Plot
+  ax.set_title(label) #Set Headline to predicted label
+
+  #Hide numbers on axes
+  plt.gca().axes.get_yaxis().set_visible(False)
+  plt.gca().axes.get_xaxis().set_visible(False)
