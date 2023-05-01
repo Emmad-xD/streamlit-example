7922bb4892928121d5ed63eed2f962800c6a1f00
Emmad <62903078+Emmad-xD@users.noreply.github.com>
2023-05-02 03:16:38 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,104 +1,26 @@
-!pip install -Uqq fastai 
+import streamlit as st
 from fastai.vision.all import *
-
-"""At first we are downloading the Food Dataset, containing about 100k images of various kind of foods.
-Documentation for all Datasets: https://docs.fast.ai/data.external
-"""
-
-foodPath = untar_data(URLs.FOOD)
-
-#How many images are we dealing with
-len(get_image_files(foodPath))
-
-#Use Pandas to parse the JSON as a human readable table
-pd.read_json('/root/.fastai/data/food-101/test.json')
-
-#Deciding which two foods we want to classify
-labelA = 'samosa'
-labelB = 'hot_and_sour_soup'
-
-"""**Some Data Preperation**
-
-Next we're gonna do some preperation to make the data handling easier. It's not important that you understand the following code snippet completly.
-
-We're just doing two things:
-
-
-1.   Removing all images that don't have labelA or labelB
-2.   Renaming images with labelA or labelB to have that label in their filename
-
-
-"""
-
-#Loop through all Images downloaded
-for img in get_image_files(foodPath):    
-  #Rename Images so that the Label (Samosa or Churros) is in the file name
-  if labelA in str(img):
-    img.rename(f"{img.parent}/{labelA}-{img.name}")
-  elif labelB in str(img):
-    img.rename(f"{img.parent}/{labelB}-{img.name}")
-  else: os.remove(img) #If the Images are not part of labelA or labelB
-
-len(get_image_files(foodPath))
-
-"""## Train Model"""
 
 def GetLabel(fileName):
   return fileName.split('-')[0]
 
-GetLabel("churros-734186.jpg") #Testing
+learn = load_learner('path/to/model.pkl')
 
-dls = ImageDataLoaders.from_name_func(
-    foodPath, get_image_files(foodPath), valid_pct=0.2, seed=420,
-    label_func=GetLabel, item_tfms=Resize(32))
+def app():
+  st.set_page_config(page_title="Food Classifier", page_icon=":pizza:")
 
-dls.train.show_batch()
+  st.title("Food Classifier")
+  st.write("Upload an image of either samosa or nachos to get a prediction")
 
-learn = cnn_learner(dls, resnet34, metrics=error_rate, pretrained=True)
-learn.fine_tune(epochs=1)
+  uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-"""Check out this while training to have look inside a Convolutional Neural Network.
+  if uploaded_file is not None:
+    image = PILImage.create(uploaded_file)
+    st.image(image, caption="Uploaded image", use_column_width=True)
+    label, _, probs = learn.predict(image)
+    st.write(f"This is a {label}.")
+    st.write(f"{labelA} {probs[1].item():.6f}")
+    st.write(f"{labelB} {probs[0].item():.6f}")
 
-https://distill.pub/2017/feature-visualization/
-
-## Verify Model
-
-### Upload your own Images
-"""
-
-from google.colab import files
-uploader = files.upload()
-
-for img in uploader.items():
-  uploadedImg = img[0]
-
-img = PILImage.create(uploadedImg)
-img.show()
-
-label,_,probs = learn.predict(img)
-
-print(f"This is a {label}.")
-print(f"{labelA} {probs[1].item():.6f}")
-print(f"{labelB} {probs[0].item():.6f}")
-
-"""### Test using images in dataset"""
-
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-
-for i in range(0,10):
-  #Load random image
-  randomIndex = random.randint(0, len(get_image_files(foodPath))-1)
-  img = mpimg.imread(get_image_files(foodPath)[randomIndex])
-  #Put into Model
-  label,_,probs = learn.predict(img)
-
-  #Create Figure using Matplotlib
-  fig = plt.figure()
-  ax = fig.add_subplot() #Add Subplot (For multiple images)
-  imgplot = plt.imshow(img) #Add Image into Plot
-  ax.set_title(label) #Set Headline to predicted label
-
-  #Hide numbers on axes
-  plt.gca().axes.get_yaxis().set_visible(False)
-  plt.gca().axes.get_xaxis().set_visible(False)
+if __name__ == "__main__":
+  app()
