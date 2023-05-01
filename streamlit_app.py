68215baaf1eae66adff30a8b4a855dba4a126a4c
Emmad <62903078+Emmad-xD@users.noreply.github.com>
2023-05-02 03:22:29 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,33 @@
 import streamlit as st
 from fastai.vision.all import *
+from PIL import Image
 
-def GetLabel(fileName):
-  return fileName.split('-')[0]
+# Define the labels
+labels = ['samosa', 'hot_and_sour_soup']
 
-learn = load_learner('model.py')
+# Load the model
+learn = load_learner('model.pkl')
 
+# Define the predict function
+def predict(image):
+    img = PILImage.create(image)
+    pred, pred_idx, probs = learn.predict(img)
+    return labels[pred_idx], probs[pred_idx]
+
+# Create the Streamlit app
 def app():
-  st.set_page_config(page_title="Food Classifier", page_icon=":pizza:")
+    st.title("Food Classifier")
 
-  st.title("Food Classifier")
-  st.write("Upload an image of either samosa or nachos to get a prediction")
+    # Create a file uploader widget
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-  uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    # Make a prediction if an image has been uploaded
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded image.', use_column_width=True)
+        label, prob = predict(image)
+        st.write(f"Prediction: {label}; Probability: {prob:.4f}")
 
-  if uploaded_file is not None:
-    image = PILImage.create(uploaded_file)
-    st.image(image, caption="Uploaded image", use_column_width=True)
-    label, _, probs = learn.predict(image)
-    st.write(f"This is a {label}.")
-    st.write(f"{labelA} {probs[1].item():.6f}")
-    st.write(f"{labelB} {probs[0].item():.6f}")
-
-if __name__ == "__main__":
-  app()
+# Run the app
+if __name__ == '__main__':
+    app()
